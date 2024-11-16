5691a3dc9713cada8add075bb1cd9be333199f1a
Mykaala Firdaus <mykal.kd@gmail.com>
2024-11-16 16:59:27 -0500

    Create navbar-import.js
    Created js script for modular navbar integration

---
+++ navbar/navbar-import.js
@@ -0,0 +1,7 @@
+fetch('../navbar/navbar.html')
+	.then((response) => response.text())
+	.then((data) => {
+		document.getElementById('navbar-container').innerHTML = data;
+	})
+	.catch((error) => console.error('Error loading navbar:', error));
+//the fetch route might need to be updated depending on how you guys structured your file directory
