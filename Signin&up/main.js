f21aa3e6bda4a299fd0b94e77ca897709f985797
Aryanvakil <aryanvkl82@gmail.com>
2024-11-17 11:53:27 -0500

    Added nav bar used in other pages, fixed some issues in design, added functions to utilts.js to check the inputs.

--- Signin&up/main.js
+++ Signin&up/main.js
@@ -1,9 +1,19 @@
+import {passwordIsValid, validEmailFormat, passwordsMatch, validPhoneFormat, validUsername} from './utils.js';
+
 const signInTog = document.getElementById('signInTog');
 const signUpTog = document.getElementById('signUpTog');
 const toggleBar = document.getElementById('toggleBar');
 const signInContainer = document.getElementById('signInContainer');
 const signUpContainer = document.getElementById('signUpContainer');
-
+const passwordUpInput = document.getElementById('password-input-up');
+const emailInput = document.getElementById('email-input');
+const repeatedPassword = document.getElementById('password-reinput');
+const phoneInput = document.getElementById('phone-input');
+const usernameUp = document.getElementById('username-input-up');
+const signInButton = document.getElementById('sign-in-button');
+const usernameIn = document.getElementById('username-input');
+const passwordIn = document.getElementById('password-input');
+const signUpButton = document.getElementById('sign-up-button');
 
 signInTog.addEventListener('click', () => {
         toggleBar.style.left = "0%"
@@ -21,3 +31,5 @@ signUpTog.addEventListener('click', () => {
         signUpContainer.style.display = "flex";
         signInContainer.style.display = "none";
 })
+
+
