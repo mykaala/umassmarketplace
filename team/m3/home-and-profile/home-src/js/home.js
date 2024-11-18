2ca9809128819b6b0e2a5196cc18b52be3b76bb5
kevin.mathew <kevinmathew423@hotmail.com>
2024-11-17 21:26:13 -0500

    Implemented Functionality on Nav Bar to go to corresponding pages. Submit button now directs to home page.

--- team/m3/home-and-profile/home-src/js/home.js
+++ team/m3/home-and-profile/home-src/js/home.js
@@ -51,3 +51,10 @@ function displayProducts() {
 }
 
 document.addEventListener('DOMContentLoaded', displayProducts);
+
+fetch('../../navbar/navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
--- team/m3/home-and-profile/navbar/navbar-import.js
+++
@@ -1,6 +0,0 @@
-fetch('../navbar/navbar.html')
-	.then((response) => response.text())
-	.then((data) => {
-		document.getElementById('navbar-container').innerHTML = data;
-	})
-	.catch((error) => console.error('Error loading navbar:', error));
