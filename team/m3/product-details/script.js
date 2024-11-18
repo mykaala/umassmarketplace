4ead5f5d2801b2b42bb31f48ad1370101f7ac0fc
Ali Ayaan Rizvi <aliayaanrizvi@vl965-172-31-176-103.wireless.umass.edu>
2024-11-17 19:37:02 -0500

    made separate folder for product details

--- team/m3/product-details/script.js
+++ team/m3/product-details/script.js
@@ -12,4 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+fetch('../navbar/navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
+
 
