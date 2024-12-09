996fd8b03aa6df32ca997adb7fb9f591965fae91
Aryanvakil <aryanvkl82@gmail.com>
2024-12-09 09:47:10 -0500

    Router added #38: Added the router and redirected the requests about sign in and sign up to their respectable function.

---
+++ team/m4/routes/routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {signup, signin, getProfile} from "../controller/controller.js";
+import {isAuthenticated} from "../auth/middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.get("/profile", isAuthenticated, getProfile);
+
+export default router;
