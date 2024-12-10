3f332dee05708b138f576b671517de62c558d2fa
Aryanvakil <aryanvkl82@gmail.com>
2024-12-10 00:34:35 -0500

    Handling requests for sign in and up in the backend #38: Added comments to the files.

--- team/m4/auth/middleware.js
+++ team/m4/auth/middleware.js
@@ -1,3 +1,3 @@
-export const isAuthenticated = (req, res, next) => {
-    return req.isAuthenticated() ? next() : res.redirect("../../m3/Signin&up");
+export const isAuthenticated = (req, res, next) => { //checks to see if the user is authenticated using passport isAuthenticated functions.
+    return req.isAuthenticated() ? next() : res.redirect("../../m3/Signin&up"); //if yes proceeds to the next function but if not will redirect them to sign in page.
 } 
--- team/m4/routes/routes.js
+++ team/m4/routes/routes.js
@@ -2,10 +2,10 @@ import express from "express";
 import {signup, signin, getProfile} from "../controller/controller.js";
 import {isAuthenticated} from "../auth/middleware.js";
 
-const router = express.Router();
+const router = express.Router(); //creating a router instance
 
-router.post("/signup", signup);
-router.post("/signin", signin);
-router.get("/profile", isAuthenticated, getProfile);
+router.post("/signup", signup); //sends the sign up requests to the sign up function implemented in controller/controller.js
+router.post("/signin", signin); //sends the sign in requests to the sign in function implemented in controller/controller.js
+router.get("/profile", isAuthenticated, getProfile); //if the profile access has been requested checks to see if the user is authenticated first.
 
-export default router;
+export default router; //exporting the router to be used in other files
