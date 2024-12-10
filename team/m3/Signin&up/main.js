0bf6a2b77dc7b99c51092869f4ee3b6fa0d556a3
Aryanvakil <aryanvkl82@gmail.com>
2024-12-10 00:30:16 -0500

    FrontEnd update for sign in and sign up #36: added comments to main.js and utils.js and fixing a minor typo in utils.js

--- team/m3/Signin&up/main.js
+++ team/m3/Signin&up/main.js
@@ -1,21 +1,21 @@
 import {passwordIsValid, validEmailFormat, passwordsMatch, validPhoneFormat, validUsername, signInBackEnd, signUpBackEnd} from './utils.js';
+//initializing variables for each of the DOM elements that are interactive in the sign in/up page
+const signInTog = document.getElementById('signInTog'); //toggle to choose sign in/up, to show sign in
+const signUpTog = document.getElementById('signUpTog'); //toggle to choose sign in/up, to show sign up
+const toggleBar = document.getElementById('toggleBar'); //the toggleBar itself
+const signInContainer = document.getElementById('signInContainer'); //container form for sign in 
+const signUpContainer = document.getElementById('signUpContainer'); //container form for sign up
+const passwordUpInput = document.getElementById('password-input-up'); //password input textbox for the sign up page
+const emailInput = document.getElementById('email-input'); //email input textbox for the sign up page
+const repeatedPassword = document.getElementById('password-reinput'); //password repeated input textbox for the sign up page
+const phoneInput = document.getElementById('phone-input'); //phone number input textbox for the sign up page
+const usernameUp = document.getElementById('username-input-up'); //username input textbox for sign up page
+const signInButton = document.getElementById('sign-in-button'); //sign in button
+const usernameIn = document.getElementById('username-input'); //username input textbox for sign in page
+const passwordIn = document.getElementById('password-input'); //password input textbox for sign in page
+const signUpButton = document.getElementById('sign-up-button'); //sign up button
 
-const signInTog = document.getElementById('signInTog');
-const signUpTog = document.getElementById('signUpTog');
-const toggleBar = document.getElementById('toggleBar');
-const signInContainer = document.getElementById('signInContainer');
-const signUpContainer = document.getElementById('signUpContainer');
-const passwordUpInput = document.getElementById('password-input-up');
-const emailInput = document.getElementById('email-input');
-const repeatedPassword = document.getElementById('password-reinput');
-const phoneInput = document.getElementById('phone-input');
-const usernameUp = document.getElementById('username-input-up');
-const signInButton = document.getElementById('sign-in-button');
-const usernameIn = document.getElementById('username-input');
-const passwordIn = document.getElementById('password-input');
-const signUpButton = document.getElementById('sign-up-button');
-
-signInTog.addEventListener('click', () => {
+signInTog.addEventListener('click', () => { //functionality of the toggle for choosing sign in or sign up implemented here. In this instance sign in page is being shown.
         toggleBar.style.left = "0%"
         toggleBar.style.color = "white";
         toggleBar.innerHTML = "Sign In";
@@ -24,7 +24,7 @@ signInTog.addEventListener('click', () => {
 })
 
 
-signUpTog.addEventListener('click', () => {
+signUpTog.addEventListener('click', () => { //functionality of the toggle for choosing sign in or sign up implemented here. In this instance sign up page is being shown.
         toggleBar.style.left = "50%";
         toggleBar.style.color = "white";
         toggleBar.innerHTML = "Sign Up";
@@ -34,111 +34,111 @@ signUpTog.addEventListener('click', () => {
 
 
 passwordUpInput.addEventListener("input", () => {
-    if (passwordIsValid(passwordUpInput.value)) { 
-        document.getElementById('passwordFeedback-up').style.display = "none";
+    if (passwordIsValid(passwordUpInput.value)) { //checking to see if the password entered is valid in the sign up page
+        document.getElementById('passwordFeedback-up').style.display = "none"; //if yes removing any feedback that may have existed before.
         document.getElementById('passwordFeedback-up').innerHTML = "";
-        if (!passwordsMatch(passwordUpInput.value, repeatedPassword.value)) {
-            document.getElementById('confirmerFeedback').style.display = "block";
+        if (!passwordsMatch(passwordUpInput.value, repeatedPassword.value)) { //checking to see if the password and repeated password match
+            document.getElementById('confirmerFeedback').style.display = "block"; //if not, providing feedback under the repeat password textbox letting the user know
             document.getElementById('confirmerFeedback').innerHTML = "Passwords Don't Match.";
         }
         else {
-            document.getElementById('confirmerFeedback').style.display = "none";
+            document.getElementById('confirmerFeedback').style.display = "none"; //if they match removing any feedback that may have existed before under repeat password.
             document.getElementById('confirmerFeedback').innerHTML = "";
         }
     }
-    else {
-        document.getElementById('passwordFeedback-up').style.display = "block";
+    else { //if the password does not meet the requirements, providing feedback letting the user know about all of the requirements.
+        document.getElementById('passwordFeedback-up').style.display = "block"; 
         document.getElementById('passwordFeedback-up').innerHTML = "Your password should be at least 8 characters long and contain at least one number, alphabet, and special character!";
     }
 }
 )
 
 emailInput.addEventListener("input", () => {
-    if (validEmailFormat(emailInput.value)) {
-        document.getElementById('emailFeedback').style.display = "none";
-        document.getElementById('emailFeedback').innerHTML = "";
+    if (validEmailFormat(emailInput.value)) { //checking the inputted email format. 
+        document.getElementById('emailFeedback').style.display = "none"; //if it satisfies all the requirements, removing all of the feedbacks that may have existed under
+        document.getElementById('emailFeedback').innerHTML = ""; //the email textbox.
     }
-    else {
+    else { //if it doesn't meet the requirements, umass domain email in this case, providing feedback to user under the email input textbox.
         document.getElementById('emailFeedback').style.display = "block";
         document.getElementById('emailFeedback').innerHTML = "Email should be of example@umass.edu format.";
     }
 })
 
-repeatedPassword.addEventListener("input", () => {
-    if (passwordsMatch(passwordUpInput.value, repeatedPassword.value)) {
-        document.getElementById('confirmerFeedback').style.display = "none";
+repeatedPassword.addEventListener("input", () => { //whenever the repeated password textbox is changed, checking to make sure that it still matches the password entered.
+    if (passwordsMatch(passwordUpInput.value, repeatedPassword.value)) { //This is to stop people from changing this field after matching it initially.
+        document.getElementById('confirmerFeedback').style.display = "none"; //if it does removing any feedbacks that may have existed under the repeated password textbox
         document.getElementById('confirmerFeedback').innerHTML = "";
     }
-    else {
+    else { //if they don't match, add a feedback under the repeated password input textbox letting the user know that the passwords don't match.
         document.getElementById('confirmerFeedback').style.display = "block";
         document.getElementById('confirmerFeedback').innerHTML = "Passwords Don't Match.";
     }
 })
 
-phoneInput.addEventListener("input", () => {
-    if (validPhoneFormat(phoneInput.value)) {
+phoneInput.addEventListener("input", () => { //making sure that the phone number inputted consists of all numbers and of the right format.
+    if (validPhoneFormat(phoneInput.value)) { //if yes, removing any feedback that may have existed in the feedback placeholder under the phone number textbox.
         document.getElementById('phoneFeedback').style.display = "none";
         document.getElementById('phoneFeedback').innerHTML = "";
     }
-    else {
+    else { //if not, providing feedback under the phone input textbox to the let the user know about the requirements.
         document.getElementById('phoneFeedback').style.display = "block";
         document.getElementById('phoneFeedback').innerHTML = "Phone should be of the format (123) 456-7890.";
     }
 })
 
 usernameUp.addEventListener("input", () => {
-    if (validUsername(usernameUp.value)) {
-        document.getElementById('usernameFeedback-up').style.display = "none";
+    if (validUsername(usernameUp.value)) { //checking the username for the sign up page to make sure it meets the requirements
+        document.getElementById('usernameFeedback-up').style.display = "none"; //if it does, then removing any feedback that may have existed under the input textbox
         document.getElementById('usernameFeedback-up').innerHTML = "";
     }
-    else {
+    else { //if it doesn't displaying a feedback under the username field to let the user know about the requirements.
         document.getElementById('usernameFeedback-up').style.display = "block";
         document.getElementById('usernameFeedback-up').innerHTML = "Username should not be empty and it should only contain alphanumerical characters, dot, and underscore.";
     }
 })
 
-signInButton.addEventListener('click', () => {
+signInButton.addEventListener('click', () => { //when the sign in button is clicked
     const signInContainer = document.getElementById('signInContainer');
     const inputs = signInContainer.querySelectorAll('.textBox');
     const feedbacks = signInContainer.querySelectorAll('.feedback');
-    inputs.forEach((input, index) => {
+    inputs.forEach((input, index) => { //check to see if any of the fields are empty
         const feedback = feedbacks[index];
-        if (input.value.trim() === '') {
+        if (input.value.trim() === '') { //if there are any letting the user know that they are mandatory
             feedback.innerHTML = "This field is required.";
             feedback.style.display = "block";
         }
         else {
-            feedback.innerHTML = "";
+            feedback.innerHTML = ""; //if not removing any that may have existed
             feedback.style.display = "none";
         }
     })
-    if (Array.from(feedbacks).filter(feedback => feedback.style.display === 'none').length === feedbacks.length) {
+    if (Array.from(feedbacks).filter(feedback => feedback.style.display === 'none').length === feedbacks.length) { //making sure that the inputs meet the requirements as thats the only way for the feedback fields to be empty
         //alert(`This feature has not been implemented yet as it requires back-end code. Thank you for your patience while we work on it.`);
-        signInBackEnd(usernameIn.value, passwordIn.value);
-        passwordIn.value = '';
+        signInBackEnd(usernameIn.value, passwordIn.value); //calling the signInBackEnd functions to connect to backend and sign in
+        passwordIn.value = ''; //resetting the input fields
         usernameIn.value = '';
     }
 })
 
-signUpButton.addEventListener('click', () => {
+signUpButton.addEventListener('click', () => { //when the sign up button is clicked
     const signUpContainer = document.getElementById('signUpContainer');
     const inputs = signUpContainer.querySelectorAll('.textBox');
     const feedbacks = signUpContainer.querySelectorAll('.feedback');
-    inputs.forEach((input, index) => {
+    inputs.forEach((input, index) => { //check to see if any of the fields are empty
         const feedback = feedbacks[index];
-        if (input.value.trim() === '') {
+        if (input.value.trim() === '') { //if there are any letting the user know that they are mandatory
             feedback.innerHTML = "This field is required.";
             feedback.style.display = "block";
         }
-        else if (feedback.innerHTML == "This field is required." || feedback.innerHTML == "") {
+        else if (feedback.innerHTML == "This field is required." || feedback.innerHTML == "") { //if not removing any that may have existed
             feedback.innerHTML = "";
             feedback.style.display = "none";
         }
     })
-    if (Array.from(feedbacks).filter(feedback => feedback.style.display === 'none').length === feedbacks.length) {
+    if (Array.from(feedbacks).filter(feedback => feedback.style.display === 'none').length === feedbacks.length) { //making sure that the inputs meet the requirements as thats the only way for the feedback fields to be empty
         // alert(`This feature has not been implemented yet as it requires back-end code. Thank you for your patience while we work on it.`);
-        signUpBackEnd(usernameUp.value, passwordUpInput.value, document.getElementById('venmo-input').value, phoneInput.value, emailInput.value);
-        passwordUpInput.value = '';
+        signUpBackEnd(usernameUp.value, passwordUpInput.value, document.getElementById('venmo-input').value, phoneInput.value, emailInput.value); //calling the signUpBackEnd functions to connect to backend and sign up
+        passwordUpInput.value = ''; //resetting the input fields
         usernameUp.value = '';
         repeatedPassword.value = '';
         document.getElementById('venmo-input').value = '';
--- team/m3/Signin&up/utils.js
+++ team/m3/Signin&up/utils.js
@@ -20,51 +20,47 @@ function passwordsMatch(password, repeatedPassword) {
 
 function validEmailFormat(email) {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@umass\.edu$/.test(email);
-  return emailRegex;
-}
-
-function usernameMatchesPassword(username, password) {
-    //checks to see if the entered password is correct for the username entered based on the database.
+  return emailRegex; //check to make sure that the email address belongs to UMass
 }
 
 function validPhoneFormat(phoneNumber) {
     const phoneRegex = /^\((\d{3})\) \d{3}-\d{4}$/.test(phoneNumber);
-    return phoneRegex
+    return phoneRegex //checks to make sure that the phone number entered is all numbers in the form of (123) 456-7890
 }
 
 function validUsername(username) {
     if (username.length == 0) {
-        return false;
+        return false; //checks to make sure the username field is not empty
     }
-    return /^[a-zA-Z0-9._]+$/.test(username);
+    return /^[a-zA-Z0-9._]+$/.test(username); //checks to make sure the username only contains letters, numbers, dot, and underscore
 }
 
-async function signInBackEnd(username, password) {
-    const response = await fetch("/signin", {
+async function signInBackEnd(username, password) { 
+    const response = await fetch("/signin", { //sending a request for sign in to the backEnd using post method with username and password in the body
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({username, password})
     })
-    if (response.status === 200) {
+    if (response.status === 200) { //if the sign in is successful redirect to them homepage
         window.location.href = "../home-and-profile/home-src";
     }
     else {
-        alert(response.statusText);
+        alert(response.statusText); //if the username is invalid show the error encountered to the user
     }
 }
 
 async function signUpBackEnd(username, password, email, phoneNumber, venmo) {
-    const response = await fetch("/signin", {
+    const response = await fetch("/signup", { //sending a request for sign up to the backEnd using post method with username, password, venmo, phone, and email in the body
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({username, password, email, phoneNumber, venmo})
     })
     if (response.status === 200) {
-        window.location.href = "../home-and-profile/home-src";
+        window.location.href = "../home-and-profile/home-src"; //if the sign in is successful redirect to them homepage
     }
     else {
-        alert(response.statusText);
+        alert(response.statusText); //if the username is invalid show the error encountered to the user
     }
 }
-
+//exporting functions to be used in main.js file
 export {passwordIsValid, validEmailFormat, passwordsMatch, validPhoneFormat, validUsername, signInBackEnd, signUpBackEnd};
