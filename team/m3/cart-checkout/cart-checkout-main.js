17ef32ec9e4697f53ee570bd39888d9640c4b5a4
ryanguyen <rqnguyen@umass.edu>
2024-12-10 18:36:07 -0500

    fixed css

--- team/m3/cart-checkout/cart-checkout-main.js
+++ team/m3/cart-checkout/cart-checkout-main.js
@@ -157,3 +157,11 @@ document.addEventListener("DOMContentLoaded", function () {
         confirmOrderButton.addEventListener("click", proceedToCheckout); 
     }
 });
+
+fetch('../navbar/navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
+
