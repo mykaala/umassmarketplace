79678b7a4b142a8dcfeebaf87575be29efaebeae
kevin.mathew <kevinmathew423@hotmail.com>
2024-12-10 17:41:39 -0500

    Updated back-end post by allocating routes to express-products.js

--- team/m4/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); // Import Express library
-const { createProduct } = require('../controllers/productController'); // We'll create this controller next
-
-const router = express.Router();
-
-// Route for creating a product
-router.post('/products', createProduct);
-
-module.exports = router; // Export the router
